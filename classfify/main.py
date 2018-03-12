f6f9493774a1b83d71d679427cea2cbae4aa3806
ssy <sunsiyuan@qq.com>
2018-03-12 21:47:20 +0800

    fix training parameter

--- classfify/main.py
+++ classfify/main.py
@@ -2,12 +2,14 @@ from dataloader import prepare_data
 from cnn_model import cnn_model
 from keras import backend as K
 import numpy as np
+import sys
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 from time import  time
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
 # def my_init(shape, name=None):
 #     value = np.random.random(shape)
 #     return K.variable(value, name=name)
@@ -16,7 +18,7 @@ from keras.preprocessing.image import ImageDataGenerator
 def train():
     lr = 0.002
     batch_size = 16
-    epoch = 60
+    epoch = 80
     start = time()
     sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
     model = cnn_model()
@@ -27,15 +29,15 @@ def train():
 
     datagen = ImageDataGenerator(featurewise_center=False,
                                  featurewise_std_normalization=False,
-                                 width_shift_range=0.1,
-                                 height_shift_range=0.1,
+                                 width_shift_range=0.15,
+                                 height_shift_range=0.15,
                                  zoom_range=0.2,
-                                 shear_range=0.1,
+                                 shear_range=0.15,
                                  rotation_range=10., )
     datagen.fit(X_train)
 
     history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size),
-                            steps_per_epoch=1000,
+                            steps_per_epoch=2000,
                             epochs=epoch,
                             validation_data=(X_val, y_val),
                             callbacks=[ReduceLROnPlateau('val_loss', factor=0.2, patience=20, verbose=1, mode='auto'),
@@ -45,10 +47,23 @@ def train():
     print (end - start)
 
 def test():
-    pass
+    X_train, y_train, X_val, y_val, X_test, y_test = prepare_data()
+    start = time()
+    model = load_model('model.h5')
+    pred = model.predict(X_test,batch_size=1000)
+    end=time()
+    y_pred = np.empty(12630)
+    for i in range(0,12630):
+        y_pred[i] = (np.argmax(pred[i][:]))
+    acc = np.mean(y_pred==y_test)
+    print("Test accuracy = {}".format(acc))
+    print  (end-start)
 
-
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-set_session(tf.Session(config=config))
-train()
+if __name__ == '__main__':
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    set_session(tf.Session(config=config))
+    if sys.argv[1] == 'train':
+        train()
+    if sys.argv[1] =='test':
+        test()
